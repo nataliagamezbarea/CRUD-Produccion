5fa0b0cedc7037e86ba7d78bd70115357b888ddf
nataliagamezbarea <nataliagbarea@gmail.com>
2024-04-04 11:27:26 +0200

    Centrado

--- fronted/src/renderer/src/components/Datos.jsx
+++ fronted/src/renderer/src/components/Datos.jsx
@@ -97,8 +97,9 @@ function Datos({ modalOpen }) {
   }
 
   return (
-    <div className="container mt-5">
-      <div className="flex space-x-4">
+    <div className="flex justify-center items-center h-screen">
+      <div className="container mt-5">
+        <div className='flex space-x-4'>
         <input
           className='class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"'
           type="text"
@@ -231,6 +232,7 @@ function Datos({ modalOpen }) {
         </div>
       )}
       {usuarioSeleccionado && <FormEditar usuarioSeleccionado={usuarioSeleccionado} />}
+    </div>
     </div>
   )
 }
