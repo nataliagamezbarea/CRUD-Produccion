9b4525a5f9daf41a0e1594a6b703aab7ab6622c7
manurgz10 <manurgz10@gmail.com>
2024-04-04 14:56:00 +0200

    merge

--- fronted/src/renderer/src/components/Login.jsx
+++ fronted/src/renderer/src/components/Login.jsx
@@ -31,10 +31,6 @@ export default function Login({ onLogin }) {
   }
 
   return (
-<<<<<<< HEAD
-=======
-
->>>>>>> dbf7e0b660deeb2192e1984c369abe3c5b9e197a
 <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
 <div className="relative py-3 sm:max-w-xl sm:mx-auto">
 <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-300 to blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
@@ -42,10 +38,7 @@ export default function Login({ onLogin }) {
 <div className='relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20'>
 <div className='max-w-md mx-auto'>
 <div>
-<<<<<<< HEAD
-=======
           </div>
->>>>>>> dbf7e0b660deeb2192e1984c369abe3c5b9e197a
         <h1 className="text-2xl font-semibold text-gray-900 dark:text-white mb-5 text-center">
           Login
         </h1>
@@ -74,10 +67,6 @@ export default function Login({ onLogin }) {
       </div>
     </div>
     </div>
-<<<<<<< HEAD
-    </div>
-=======
->>>>>>> dbf7e0b660deeb2192e1984c369abe3c5b9e197a
 
   )
 }
