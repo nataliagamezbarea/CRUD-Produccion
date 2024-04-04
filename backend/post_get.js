d652a5d5136b9697c59941e2d54af4cbe36603ed
manurgz10 <manurgz10@gmail.com>
2024-04-04 12:02:12 +0200

    Bcrypt passwords

--- backend/post_get.js
+++ backend/post_get.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+const salt = 10;
 
 const app = express();
 const port = 3000;
@@ -9,34 +11,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.options('/usuarios/:id', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).end();
+app.options("/usuarios/:id", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).end();
 });
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', (req, res) => {
-    const nombre = req.body.nombre;
-    const apellido = req.body.apellido;
-    const email = req.body.email;
-    const password = req.body.password;
+app.post("/", (req, res) => {
+  const nombre = req.body.nombre;
+  const apellido = req.body.apellido;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    let db = new sqlite3.Database('registros.db');
+  let db = new sqlite3.Database("registros.db");
 
-    db.serialize(() => {
-        db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             name TEXT,
             surname TEXT,
@@ -44,172 +51,205 @@ app.post('/', (req, res) => {
             password TEXT
         )`);
 
-        db.run(`INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)`, [nombre, apellido, email, password], function(err) {
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log(`Se ha insertado un nuevo usuario con el ID ${this.lastID}`);
-            res.redirect('http://localhost:5173/');
-        });
-
-        db.close();
-    });
-});
-
-app.post('/register', (req, res) => {
-    const { nombre, apellido, email, password } = req.body;
-
-    let db = new sqlite3.Database('registros.db');
-
-    let sql = 'INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)';
-    const params = [nombre, apellido, email, password];
-
-    db.run(sql, params, function(err) {
+    db.run(
+      `INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)`,
+      [nombre, apellido, email, password],
+      function (err) {
         if (err) {
-            console.error(err.message);
-            res.status(500).send('Error al insertar usuario en la base de datos');
-            return;
+          return console.error(err.message);
         }
+        console.log(
+          `Se ha insertado un nuevo usuario con el ID ${this.lastID}`
+        );
+        res.redirect("http://localhost:5173/");
+      }
+    );
 
-        res.send({ id: this.lastID });
-
-        // Cerrar la conexión a la base de datos después de que se haya completado la inserción
-        db.close((err) => {
-            if (err) {
-                console.error(err.message);
-            }
-            console.log('Conexión a la base de datos cerrada');
-        });
-    });
+    db.close();
+  });
 });
 
+app.post("/register", (req, res) => {
+  const { nombre, apellido, email, password } = req.body;
 
-app.get('/usuarios', (req, res) => {
-    const { id, nombre, apellido, password } = req.query;
+  let db = new sqlite3.Database("registros.db");
 
-    let db = new sqlite3.Database('registros.db');
-
-    let sql = 'SELECT * FROM users WHERE 1=1';
-    const params = [];
-
-    if (id) {
-        sql += ' AND id = ?';
-        params.push(id);
+  bcrypt.hash(password.toString(), salt, (err, hash) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error al encriptar la contraseña");
+      return;
     }
 
-    if (nombre) {
-        sql += ' AND name LIKE ?';
-        params.push('%' + nombre + '%');
-    }
+    let sql =
+      "INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)";
+    const params = [nombre, apellido, email, hash];
+    db.run(sql, params, function (err) {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Error al insertar usuario en la base de datos");
+        return;
+      }
 
-    if (apellido) {
-        sql += ' AND surname LIKE ?';
-        params.push('%' + apellido + '%');
-    }
-    
-    if (password) {
-        sql += ' AND password = ?';
-        params.push(password);
-    }
+      res.send({ id: this.lastID });
 
-    db.serialize(() => {
-        db.all(sql, params, (err, rows) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send('Error al recuperar usuarios de la base de datos');
-                return;
-            }
-            res.send(rows);
-        });
-        db.close();
+      // Cerrar la conexión a la base de datos después de que se haya completado la inserción
+      db.close((err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log("Conexión a la base de datos cerrada");
+      });
     });
+  });
 });
 
-app.get('/usuarios/:id', (req, res) => {
-    const id = req.params.id;
+app.get("/usuarios", (req, res) => {
+  const { id, nombre, apellido, password } = req.query;
 
-    let db = new sqlite3.Database('registros.db');
+  let db = new sqlite3.Database("registros.db");
 
-    db.serialize(() => {
-        db.get(`SELECT * FROM users WHERE id = ?`, id, (err, row) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send(`Error al recuperar usuario con ID ${id}`);
-                return;
-            }
-            res.send(row);
-        });
-        db.close();
+  let sql = "SELECT * FROM users WHERE 1=1";
+  const params = [];
+
+  if (id) {
+    sql += " AND id = ?";
+    params.push(id);
+  }
+
+  if (nombre) {
+    sql += " AND name LIKE ?";
+    params.push("%" + nombre + "%");
+  }
+
+  if (apellido) {
+    sql += " AND surname LIKE ?";
+    params.push("%" + apellido + "%");
+  }
+
+  if (password) {
+    sql += " AND password = ?";
+    params.push(password);
+  }
+
+  db.serialize(() => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Error al recuperar usuarios de la base de datos");
+        return;
+      }
+      res.send(rows);
     });
+    db.close();
+  });
 });
 
-app.delete('/usuarios/:id', (req, res) => {
-    const id = req.params.id;
+app.get("/usuarios/:id", (req, res) => {
+  const id = req.params.id;
 
-    let db = new sqlite3.Database('registros.db');
+  let db = new sqlite3.Database("registros.db");
 
-    db.serialize(() => {
-        db.run(`DELETE FROM users WHERE id = ?`, id, function(err) {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send('Error al borrar usuario de la base de datos');
-                return;
-            }
-            console.log(`Usuario con ID ${id} eliminado`);
-            res.send(`Usuario con ID ${id} eliminado correctamente`);
-        });
-        db.close();
+  db.serialize(() => {
+    db.get(`SELECT * FROM users WHERE id = ?`, id, (err, row) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send(`Error al recuperar usuario con ID ${id}`);
+        return;
+      }
+      res.send(row);
     });
+    db.close();
+  });
 });
 
-app.put('/usuarios/:id/update', (req, res) => {
-    const id = req.params.id;
-    const nombre = req.body.nombre;
-    const apellido = req.body.apellido;
-    const email = req.body.email;
+app.delete("/usuarios/:id", (req, res) => {
+  const id = req.params.id;
 
-    let db = new sqlite3.Database('registros.db');
+  let db = new sqlite3.Database("registros.db");
 
-    db.serialize(() => {
-        db.run(`UPDATE users SET name = ?, surname = ?, email = ? WHERE id = ?`, [nombre, apellido, email, id], function(err) {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send(`Error al actualizar usuario con ID ${id}`);
-                return;
-            }
-            console.log(`Usuario con ID ${id} actualizado`);
-            res.send(`Usuario con ID ${id} actualizado correctamente`);
-        });
-        db.close();
+  db.serialize(() => {
+    db.run(`DELETE FROM users WHERE id = ?`, id, function (err) {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Error al borrar usuario de la base de datos");
+        return;
+      }
+      console.log(`Usuario con ID ${id} eliminado`);
+      res.send(`Usuario con ID ${id} eliminado correctamente`);
     });
+    db.close();
+  });
+});
+
+app.put("/usuarios/:id/update", (req, res) => {
+  const id = req.params.id;
+  const nombre = req.body.nombre;
+  const apellido = req.body.apellido;
+  const email = req.body.email;
+
+  let db = new sqlite3.Database("registros.db");
+
+  db.serialize(() => {
+    db.run(
+      `UPDATE users SET name = ?, surname = ?, email = ? WHERE id = ?`,
+      [nombre, apellido, email, id],
+      function (err) {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send(`Error al actualizar usuario con ID ${id}`);
+          return;
+        }
+        console.log(`Usuario con ID ${id} actualizado`);
+        res.send(`Usuario con ID ${id} actualizado correctamente`);
+      }
+    );
+    db.close();
+  });
 });
 
 /*
 Api Login Manu
 */
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     const { email, password } = req.body;
-
-    let db = new sqlite3.Database('registros.db');
-
+  
+    let db = new sqlite3.Database("registros.db");
+  
     db.serialize(() => {
-        db.get(`SELECT * FROM users WHERE email = ? AND password = ?`, [email, password], (err, row) => {
-            if (err) {
+      db.get(
+        `SELECT * FROM users WHERE email = ?`,
+        [email],
+        (err, row) => {
+          if (err) {
+            console.error(err.message);
+            res
+              .status(500)
+              .send("Error al recuperar usuario de la base de datos");
+            return;
+          }
+          if (row) {
+            bcrypt.compare(password, row.password, function(err, result) {
+              if (err) {
                 console.error(err.message);
-                res.status(500).send('Error al recuperar usuario de la base de datos');
+                res.status(500).send("Error al comparar contraseñas");
                 return;
-            }
-            if (row) {
+              }
+              if (result) {
                 res.send(row);
-            } else {
-                res.status(404).send('Usuario no encontrado');
-            }
-        });
-        db.close();
+              } else {
+                res.status(401).send("Contraseña incorrecta");
+              }
+            });
+          } else {
+            res.status(404).send("Usuario no encontrado");
+          }
+        }
+      );
     });
-})
+  });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
